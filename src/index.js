1445528063ae04e8f20dba6a2e03545980f0f95b
JenniKate Wallace <jennikate@gmail.com>
2022-07-04 16:15:44 +0100

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { createRoot } from "react-dom/client";
+import App from './App';
+import './App.scss';
+
+// Below is the new React18 syntax
+const rootElement = document.getElementById('app');
+const root = createRoot(rootElement);
+
+// StrictMode is a tool for highlighting potential problems in an application. 
+// Like Fragment, StrictMode does not render any visible UI. 
+// It activates additional checks and warnings for its descendants.
+// Strict mode checks run in development only
+// https://reactjs.org/docs/strict-mode.html
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Below is the original React17 & previous syntax
+// React 18 will cause your App to behave as though it's running 17
+// if you use the below
+
+
+// const root = document.getElementById('app');
+
+// ReactDOM.render(<App />, root);
